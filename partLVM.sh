6a34353a8f80012ed7780b7e229b4f10a5a056b0
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-06-30 16:47:12 +0200

    Update partLVM.sh

--- partLVM.sh
+++ partLVM.sh
@@ -1,42 +1,24 @@
-#!/bin/bash
-# Auteur: ps81frt
-DISK="/dev/sda"
-VG_NAME="ubuntu-vg"
-LV_ROOT="lvroot"
-LV_HOME="lvhome"
-LV_VAR="lvvar"
-LV_TMP="lvtmp"
-LV_SWAP="lvswap"
+sudo parted /dev/sda -- mklabel gpt
 
-echo "Effacement et partitionnement du disque $DISK"
+# EFI
+sudo parted /dev/sda -- mkpart ESP fat32 1MiB 1025MiB
+sudo parted /dev/sda -- set 1 boot on
 
-parted --script $DISK \
-  mklabel gpt \
-  mkpart primary fat32 1MiB 1025MiB \
-  set 1 boot on \
-  mkpart primary 1025MiB 100% \
-  set 2 lvm on
+# Partition LVM
+sudo parted /dev/sda -- mkpart primary 1025MiB 100%
+sudo parted /dev/sda -- set 2 lvm on
 
-pvcreate ${DISK}2
-vgcreate $VG_NAME ${DISK}2
+sudo pvcreate /dev/sda2
+sudo vgcreate ubuntu-vg /dev/sda2
+sudo lvcreate -L 60G -n lvroot ubuntu-vg
+sudo lvcreate -L 100G -n lvhome ubuntu-vg
+sudo lvcreate -L 40G -n lvvar ubuntu-vg
+sudo lvcreate -L 20G -n lvtmp ubuntu-vg
+sudo lvcreate -L 8G -n lvswap ubuntu-vg
 
-lvcreate -L 60G -n $LV_ROOT $VG_NAME
-lvcreate -L 100G -n $LV_HOME $VG_NAME
-lvcreate -L 40G -n $LV_VAR $VG_NAME
-lvcreate -L 20G -n $LV_TMP $VG_NAME
-lvcreate -L 8G -n $LV_SWAP $VG_NAME
-
-mkfs.vfat -F32 ${DISK}1
-mkfs.ext4 /dev/$VG_NAME/$LV_ROOT
-mkfs.ext4 /dev/$VG_NAME/$LV_HOME
-mkfs.ext4 /dev/$VG_NAME/$LV_VAR
-mkfs.ext4 /dev/$VG_NAME/$LV_TMP
-mkswap /dev/$VG_NAME/$LV_SWAP
-
-echo "Partition EFI : ${DISK}1"
-echo "Volumes logiques :"
-echo " Root : /dev/$VG_NAME/$LV_ROOT"
-echo " Home : /dev/$VG_NAME/$LV_HOME"
-echo " Var  : /dev/$VG_NAME/$LV_VAR"
-echo " Tmp  : /dev/$VG_NAME/$LV_TMP"
-echo " Swap : /dev/$VG_NAME/$LV_SWAP"
+sudo mkfs.vfat -F32 /dev/sda1
+sudo mkfs.ext4 /dev/ubuntu-vg/lvroot
+sudo mkfs.ext4 /dev/ubuntu-vg/lvhome
+sudo mkfs.ext4 /dev/ubuntu-vg/lvvar
+sudo mkfs.ext4 /dev/ubuntu-vg/lvtmp
+sudo mkswap /dev/ubuntu-vg/lvswap
